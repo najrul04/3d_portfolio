4875ea58cdf28959adff5273588264fa79cf738f
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-08-27 23:26:41 +0600

    name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,9 @@ const Hero = () => {
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
-
+{/* 
+name
+*/}
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Najrul</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>React Developer </p>
